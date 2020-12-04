3489f7e0f66b7549dcb529544c660afaa81e53ab
nancycmartinez <a_dramartinez@hotmail.com>
2020-12-04 17:59:17 -0600

    Validaciones de rol para Usuario y roles

--- app/Controllers/API/Roles.php
+++ app/Controllers/API/Roles.php
@@ -1,95 +1,132 @@
-<?php namespace App\Controllers\API;
+<?php
+
+namespace App\Controllers\API;
 
 use App\Models\RolModel;
 use CodeIgniter\RESTful\ResourceController;
 
-class Rol extends ResourceController
+class Roles extends ResourceController
 {
-    public function __construct(){
-		$this->model = $this->setModel(new RolModel());
-		helper('access_rol');
+  //constructor
+  public function __construct()
+  {
+    $this->model = $this->setModel(new RolModel());
+    helper('access_rol');
+  }
+
+  public function index()
+  {
+    try {
+      //validacion
+      if (!validateAccess(array('admin'), $this->request->getServer('HTTP_AUTHORIZATION')))
+        return  $this->failServerError('El rol no posee acceso al recurso!!');
+
+      //Datos de secuencia
+      $roles = $this->model->findAll();
+
+      if ($roles == null)
+        return $this->failNotFound("No se han encontrdao Registros!");
+
+      return $this->respond($roles);
+    } catch (\Exception $e) {
+      return $this->failServerError('Ha ocurriodo un error en el servidor!!!');
     }
-	public function index()
-	{
-		$Rol = $this->model->findAll();
+  }
 
-		return $this->respond($Rol);
-	}
-	public function create()
-	{
-		try {
-			$rol= $this->request->getJSON();
-			if($this->model->insert($rol)):
-				$rol->id = $this->model->insertID();
-				return $this->respondCreated($rol);
-			else:
-				return $this->failValidationError($this->model->validation->listErrors());
-			endif;
+  public function create()
+  {
+    try {
+      //validacion
+      if (!validateAccess(array('admin'), $this->request->getServer('HTTP_AUTHORIZATION')))
+        return  $this->failServerError('El rol no posee acceso al recurso!!');
 
-		} catch (\Exception $e) {
-			//throw $th;
-			return $this->failServerError('Ha ocurrido un error en el servidor');
-		}
-	}
-	public function edit($id = null)
-	{
-		try {
-			if($id==null)
-				return $this->failValidationError('No se ha pasado ID Valido');
-			$rol = $this->model->find($id);
+      //Datos de secuencia
+      $rol = $this->request->getJSON();
 
-			if($rol==null)
-				return $this->failNotFound('No se ha encontrado un Rol con id: ' .$id);
-			
-			return $this->respond($rol);
+      if ($this->model->insert($rol)) :
+        $rol->id = $this->model->insertID();
+        return $this->respondCreated($rol);
+      else :
+        return $this->failValidationError($this->model->validation->listErrors());
+      endif;
+    } catch (\Exception $e) {
+      return $this->failServerError('Ha ocurriodo un error en el servidor!!!');
+    }
+  }
 
-		} catch (\Exception $e) {
-			//throw $th;
-			return $this->failServerError('Ha ocurrido un error en el servidor');
-		}
-	}
-	public function update($id = null)
-	{
-		try {
-			if($id==null)
-				return $this->failValidationError('No se ha pasado ID Valido');
-			$verificarRol = $this->model->find($id);
+  public function edit($id = null)
+  {
+    try {
+      //validacion
+      if (!validateAccess(array('admin'), $this->request->getServer('HTTP_AUTHORIZATION')))
+        return  $this->failServerError('El rol no posee acceso al recurso!!');
 
-			if($verificarRol==null)
-				return $this->failNotFound('No se ha encontrado un Rol con id: ' .$id);
-			
-			$rol = $this->request->getJSON();
-			if($this->model->update($id,$rol)):
-				$rol->id= $id;
-				return $this->respondUpdated($rol);
-			else:
-				return $this->failValidationError($this->model->validation->listErrors());
-			endif;
+      //Datos de secuencia
+      if ($id == null)
+        return $this->failValidationError('No se ha ingresado un ID valido');
 
-		} catch (\Exception $e) {
-			//throw $th;
-			return $this->failServerError('Ha ocurrido un error en el servidor');
-		}
-	}
-	public function delete($id = null)
-	{
-		try {
-			if($id==null)
-				return $this->failValidationError('No se ha pasado ID Valido');
-			$verificarRol = $this->model->find($id);
+      $rol = $this->model->find($id);
+      if ($rol == null)
+        return $this->failNotFound('No se ha encontrado registro con el id: ' . $id);
 
-			if($verificarRol==null)
-				return $this->failNotFound('No se ha encontrado un Rol con id: ' .$id);
-			
-			if($this->model->delete($id)):
-				return $this->respondDeleted($verificarRol);
-			else:
-				return $this->failServerError('No se ha Podido borrar el Registro');
-			endif;
+      return $this->respond($rol);
+    } catch (\Exception $e) {
+      return $this->failServerError('Ha ocurriodo un error en el servidor!!!');
+    }
+  }
 
-		} catch (\Exception $e) {
-			//throw $th;
-			return $this->failServerError('Ha ocurrido un error en el servidor');
-		}
-	}
+  public function update($id = null)
+  {
+    try {
+      //validacion
+      if (!validateAccess(array('admin'), $this->request->getServer('HTTP_AUTHORIZATION')))
+        return  $this->failServerError('El rol no posee acceso al recurso!!');
+
+      //Datos de secuencia
+      if ($id == null)
+        return $this->failValidationError('No se ha ingresado un ID valido');
+
+      $rolVerificado = $this->model->find($id);
+      if ($rolVerificado == null)
+        return $this->failNotFound('No se ha encontrado registro con el id: ' . $id);
+
+      //actualizar  
+      $rol = $this->request->getJSON();
+
+      if ($this->model->update($id, $rol)) :
+        $rol->id = $id;
+        return $this->respondUpdated($rol);
+      else :
+        return $this->failValidationError($this->model->validation->listErrors());
+      endif;
+    } catch (\Exception $e) {
+      return $this->failServerError('Ha ocurriodo un error en el servidor!!!');
+    }
+  }
+
+  public function delete($id = null)
+  {
+    try {
+      //validacion
+      if (!validateAccess(array('admin'), $this->request->getServer('HTTP_AUTHORIZATION')))
+        return  $this->failServerError('El rol no posee acceso al recurso!!');
+
+      //Datos de secuencia
+      if ($id == null)
+        return $this->failValidationError('No se ha ingresado un ID valido');
+
+      $rolVerificado = $this->model->find($id);
+      if ($rolVerificado == null)
+        return $this->failNotFound('No se ha encontrado registro con el id: ' . $id);
+
+      //eliminar  
+      if ($this->model->delete($id)) :
+        return $this->respondDeleted($rolVerificado);
+      else :
+        return $this->failServerError("No se ha podido eliminar el registro!!!");
+      endif;
+    } catch (\Exception $e) {
+      return $this->failServerError('Ha ocurriodo un error en el servidor!!!');
+    }
+  }
 }
--- app/Controllers/API/Usuarios.php
+++ app/Controllers/API/Usuarios.php
@@ -1,34 +1,52 @@
-<?php namespace App\Controllers\API;
+<?php
+
+namespace App\Controllers\API;
 
 use App\Models\UsuarioModel;
 use CodeIgniter\RESTful\ResourceController;
 
 class Usuarios extends ResourceController
 {
-    public function __construct(){
+	public function __construct()
+	{
 		$this->model = $this->setModel(new UsuarioModel());
 		helper('secure_password');
 		helper('access_rol');
-    }
+	}
 	public function index()
 	{
-		$Usuarios = $this->model->findAll();
+		try {
+			//validacion de rol
+			if (!validateAccess(array('admin'), $this->request->getServer('HTTP_AUTHORIZATION')))
+				return  $this->failServerError('El rol no posee acceso al recurso!!');
 
-		return $this->respond($Usuarios);
+			//secuencia
+			$usuarios = $this->model->findAll();
+			if ($usuarios == null)
+				return $this->failNotFound("No se han encontrado registros!!");
+			return $this->respond($usuarios);
+		} catch (\Exception $e) {
+			//log_message('error','[ERROR] {exception}',['exception' => $e]);
+			return $this->failServerError('Ha ocurriodo un error en el servidor');
+		}
 	}
 	public function create()
 	{
 		try {
-			$usuario= $this->request->getJSON();
+			//validacion de rol
+			if (!validateAccess(array('admin'), $this->request->getServer('HTTP_AUTHORIZATION')))
+				return  $this->failServerError('El rol no posee acceso al recurso!!');
+
+			//Datos de secuencia
+			$usuario = $this->request->getJSON();
 			//encrita contraseña
 			$usuario->password = hashPassword($usuario->password);
-			if($this->model->insert($usuario)):
+			if ($this->model->insert($usuario)) :
 				$usuario->id = $this->model->insertID();
 				return $this->respondCreated($usuario);
-			else:
+			else :
 				return $this->failValidationError($this->model->validation->listErrors());
 			endif;
-
 		} catch (\Exception $e) {
 			//throw $th;
 			return $this->failServerError('Ha ocurrido un error en el servidor');
@@ -37,15 +55,19 @@ class Usuarios extends ResourceController
 	public function edit($id = null)
 	{
 		try {
-			if($id==null)
+			//validacion de rol
+			if (!validateAccess(array('admin'), $this->request->getServer('HTTP_AUTHORIZATION')))
+				return  $this->failServerError('El rol no posee acceso al recurso!!');
+
+			//Datos de secuencia
+			if ($id == null)
 				return $this->failValidationError('No se ha pasado ID Valido');
 			$usuario = $this->model->find($id);
 
-			if($usuario==null)
-				return $this->failNotFound('No se ha encontrado un usuario con id: ' .$id);
-			
-			return $this->respond($usuario);
+			if ($usuario == null)
+				return $this->failNotFound('No se ha encontrado un usuario con id: ' . $id);
 
+			return $this->respond($usuario);
 		} catch (\Exception $e) {
 			//throw $th;
 			return $this->failServerError('Ha ocurrido un error en el servidor');
@@ -54,24 +76,28 @@ class Usuarios extends ResourceController
 	public function update($id = null)
 	{
 		try {
-			if($id==null)
+			//validacion
+			if (!validateAccess(array('admin'), $this->request->getServer('HTTP_AUTHORIZATION')))
+				return  $this->failServerError('El rol no posee acceso al recurso!!');
+
+			//Datos de secuencia
+			if ($id == null)
 				return $this->failValidationError('No se ha pasado ID Valido');
 			$verificarUsuario = $this->model->find($id);
 
-			if($verificarUsuario==null)
-				return $this->failNotFound('No se ha encontrado un usuario con id: ' .$id);
-			
+			if ($verificarUsuario == null)
+				return $this->failNotFound('No se ha encontrado un usuario con id: ' . $id);
+
 			$usuario = $this->request->getJSON();
-			
+
 			//encripcaion de password
 			$usuario->password = hashPassword($usuario->password);
-			if($this->model->update($id,$usuario)):
-				$usuario->id= $id;
+			if ($this->model->update($id, $usuario)) :
+				$usuario->id = $id;
 				return $this->respondUpdated($usuario);
-			else:
+			else :
 				return $this->failValidationError($this->model->validation->listErrors());
 			endif;
-
 		} catch (\Exception $e) {
 			//throw $th;
 			return $this->failServerError('Ha ocurrido un error en el servidor');
@@ -80,19 +106,23 @@ class Usuarios extends ResourceController
 	public function delete($id = null)
 	{
 		try {
-			if($id==null)
+			//validacion
+			if (!validateAccess(array('admin'), $this->request->getServer('HTTP_AUTHORIZATION')))
+				return  $this->failServerError('El rol no posee acceso al recurso!!');
+
+			//Datos de secuencia
+			if ($id == null)
 				return $this->failValidationError('No se ha pasado ID Valido');
 			$verificarUsuario = $this->model->find($id);
 
-			if($verificarUsuario==null)
-				return $this->failNotFound('No se ha encontrado un Usuario con id: ' .$id);
-			
-			if($this->model->delete($id)):
+			if ($verificarUsuario == null)
+				return $this->failNotFound('No se ha encontrado un Usuario con id: ' . $id);
+
+			if ($this->model->delete($id)) :
 				return $this->respondDeleted($verificarUsuario);
-			else:
+			else :
 				return $this->failServerError('No se ha Podido borrar el Registro');
 			endif;
-
 		} catch (\Exception $e) {
 			//throw $th;
 			return $this->failServerError('Ha ocurrido un error en el servidor');
