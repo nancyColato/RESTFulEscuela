8be377cfdb6f9386bddc3167d0a3b5bff8ce53e8
nancycmartinez <a_dramartinez@hotmail.com>
2020-12-04 11:59:43 -0600

    validacion de password

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -16,11 +16,14 @@ class UsuarioModel extends Model
     protected $validationRules  = [
         'nombre'                => 'required|alpha_space|min_length[3]|max_length[65]',
         'username'              => 'required|alpha_numeric_space|min_length[3]|max_length[10]',
-        'password'              => 'required|alpha_numeric_space|min_length[5]|max_length[10]',
+        'password'              => 'required|regex_match[/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/]|min_length[8]|max_length[250]',
         'rol_id'                => 'required|is_valid_rol',
     ];
     protected $validationMessages = [
-        'rol_id'  => ['is_valid_rol' => 'El rol que intenta ingresar no existe.']
+        'rol_id'  => ['is_valid_rol' => 'El rol que intenta ingresar no existe.'],
+        'password'      => [
+            'regex_match' => 'El formato de contraseña es invalido! debe contener numeros, letras en  minusculas y mayusculas',
+        ]
     ];
 
     protected $skipValidation = false;
